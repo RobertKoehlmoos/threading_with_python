4aae120eafe8adf7aa9053435fb5719e20ffec33
rob <robert.koehlmoos@gmail.com>
2021-09-28 11:11:09 -0400

    Added netcat as a threading problem

--- problems.py
+++ problems.py
@@ -13,3 +13,18 @@ Hint: Don't be clever when checking how many characters there are in a file, jus
 
 def chars_in_sonnets() -> int:
     pass
+
+
+"""
+Don't trust weird executables you find online? Let's write our own version of netcat with a python function
+Have a the function start a connection based on it's arguments, then send user input down the connection and
+print any data it receives.
+
+No tests for this one, try using it on an echo server or any other server you've written previously.
+Hint: You'll probably need to create two threads, one for collecting and sending user input and one for
+receiving and printing messages from the connection.
+"""
+
+
+def netcat(ipaddr: str, port: int):
+    pass
